d92001675105a247c06467545b924d2240662c9c
Evan Christianson <evanjameschristianson@gmail.com>
2020-11-22 18:02:05 -0600

    Created reference links

---
+++ script.js
@@ -0,0 +1,8 @@
+// Header name
+$("#nameHeader").text("Evan Christianson");
+// Profile and contact links
+$("#linksHeader").text("Links:");
+$("#linkOne").text("GitHub");
+$("#linkTwo").text("LinkedIn");
+$("#linkThree").text("Resume");
+$("#contactHeader").text("Contact");
