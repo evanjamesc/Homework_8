59ad743fb7b847026923aeb261c78d874ae2e801
Evan Christianson <evanjameschristianson@gmail.com>
2020-11-22 19:13:12 -0600

    Links for password generator

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
+// index.html
 // Header name
 $("#nameHeader").text("Evan Christianson");
-// Profile and contact links
+
+// Profile links
 $("#linksHeader").text("Links:");
+
 $("#linkOne").text("GitHub");
+$("#linkOne").attr("href", "https://github.com/evanjamesc");
+
 $("#linkTwo").text("LinkedIn");
+$("#linkTwo").attr("href", "https://www.linkedin.com/in/evan-christianson-3a29861b6?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BAcL%2FmgHuQQugpXRzRlrAQg%3D%3D");
+
 $("#linkThree").text("Resume");
+$("#linkThree").attr("href", "https://drive.google.com/file/d/1oLIRQyj4cuZEGGmwTS3QKJZP1cZpqj9z/view?usp=sharing");
+
+// Contact links
 $("#contactHeader").text("Contact");
+$("#phoneNumber").text("(715)-821-9377");
+$("#email").text("evanjameschristianson@gmail.com");
+
+// portfolio.html
+// Password Generator
+$("#portfolioImg1").attr("src", "./assets/images/passwordGenerator.png");
+$("#projectOneTitle").text("Password Generator");
+$("#projectOneRepo").text("GitHub Repository");
+$("#projectOneRepo").attr("href", "https://github.com/evanjamesc/Homework_3");
+$("#projectOneDeployed").text("Deployed Site");
+$("#projectOneDeployed").attr("href", "https://evanjamesc.github.io/Homework_3/");
+
+// Weather Dashboard
+$("#portfolioImg2").attr("src", "./assets/images/weather.png");
+$("#projectTwoTitle").text("Weather Dashboard");
+$("#projectTwoRepo").text("GitHub Repository");
+$("#projectTwoDeployed").text("Deployed Site");
+
+// Project 1
+$("#portfolioImg3").attr("src", "./assets/images/projectOne.png");
+$("#projectThreeTitle").text("BeerDay Helper");
+$("#projectThreeRepo").text("GitHub Repository");
+$("#projectThreeDeployed").text("Deployed Site");
